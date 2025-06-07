e18052a2daa327d2e58509191624ede2044ce879
Aleksey525 <alex_tolchin@mail.ru>
2025-06-08 02:58:15 +0300

    Add user repository

---
+++ src/repositories/user_repository.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.models.users import User
+
+
+class UserRepository:
+    async def create(
+            self,
+            session: AsyncSession,
+            first_name: str,
+            last_name: str,
+            username: str,
+            hashed_password: str,
+            is_active: bool = True,
+    ) -> User:
+        user = User(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            hashed_password=hashed_password,
+            is_active=is_active,
+        )
+        session.add(user)
+        await session.commit()
+        await session.refresh(user)
+        return user
+
+    async def get_by_id(self, session: AsyncSession, user_id: int) -> Optional[User]:
+        result = await session.execute(select(User).where(User.id == user_id))
+        return result.scalar_one_or_none()
