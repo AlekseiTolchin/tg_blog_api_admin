8b157189e0feee7ed11f445e246f38756e8aa683
Aleksey525 <alex_tolchin@mail.ru>
2025-06-08 22:54:46 +0300

    Add default values

--- src/config.py
+++ src/config.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_URL = os.getenv('API_URL')
+API_URL = os.getenv('API_URL', 'http://api:8000/posts/')
 TG_BOT_TOKEN = os.getenv('TG_BOT_TOKEN')
-DATABASE_URL = os.getenv('DATABASE_URL')
-SYNC_DATABASE_URL = os.getenv('SYNC_DATABASE_URL')
+DATABASE_URL = os.getenv('DATABASE_URL', 'sqlite+aiosqlite:///posts.db')
+SYNC_DATABASE_URL = os.getenv('SYNC_DATABASE_URL', 'sqlite:///posts.db')
 JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
-JWT_ALGORITHM = os.getenv('JWT_ALGORITHM')
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES'))
-REFRESH_TOKEN_EXPIRE_DAYS = int(os.getenv('REFRESH_TOKEN_EXPIRE_DAYS'))
+JWT_ALGORITHM = os.getenv('JWT_ALGORITHM', 'HS256')
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES', 15))
+REFRESH_TOKEN_EXPIRE_DAYS = int(os.getenv('REFRESH_TOKEN_EXPIRE_DAYS', 7))
